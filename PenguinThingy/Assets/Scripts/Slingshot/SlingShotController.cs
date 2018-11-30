51e2938b8dd452a510aeb9cc9f6b3dc233c62ede
Julian Heitkamp <heitkamp.julian@googlemail.com>
2018-11-30 20:51:42 +0100

    eggs&shit

--- PenguinThingy/Assets/Scripts/Slingshot/SlingShotController.cs
+++ PenguinThingy/Assets/Scripts/Slingshot/SlingShotController.cs
@@ -11,18 +11,22 @@ public class SlingShotController : MonoBehaviour {
     [SerializeField]
     float maxSpeed = 0.0f;
     
-    public float birdAmount;
+    public int birdAmount;
     bool increasing = true;
     [SerializeField]
     Transform target;
     [SerializeField]
+    Transform birdSpawn;
+    [SerializeField]
+    Transform maxPosition;
+    [SerializeField]
     GameObject projectilePF;
     GameObject projectile;
-
+    bool shot = true;
     float sliderFill = 0f;
     [SerializeField]
     Slider slider;
-   
+    
 
     Vector3 direction;
 
@@ -35,16 +39,15 @@ public class SlingShotController : MonoBehaviour {
 	void Update () {
         if (Input.GetButton("Fire1"))
         {
-            
-            
-            // setting up Projectile + getting direction
-            
-           
-             
-             
-            
-            
 
+
+            // setting up Projectile + getting direction
+            if (shot)
+            {
+                projectile = Instantiate(projectilePF,birdSpawn.position,birdSpawn.rotation,birdSpawn);
+
+                shot = false;
+            }
             if (speed + speedPerFrame*Time.deltaTime >= maxSpeed)
             {
                 increasing = false;
@@ -58,33 +61,38 @@ public class SlingShotController : MonoBehaviour {
             if (increasing)
             {
                 speed += speedPerFrame * Time.deltaTime;
+                
             }
             else
             {
                 speed -= speedPerFrame * Time.deltaTime;
+               
             }
 
             sliderFill = Mathf.Clamp01(speed/maxSpeed   );
             Debug.Log(sliderFill);
             slider.value = sliderFill;
 
+
+
         }  
         if(Input.GetButtonUp("Fire1"))
         {
-            if (birdAmount >= 0)
+            if (birdAmount > 0)
             {
-
+                projectile.transform.SetParent(null);
                 direction = transform.position - target.position;
-                projectile = Instantiate(projectilePF, transform.position, transform.rotation);
+                
 
                 direction.Normalize();
 
 
                 Rigidbody2D rb = projectile.GetComponent<Rigidbody2D>();
-
+                rb.bodyType = RigidbodyType2D.Dynamic;
                 rb.velocity = new Vector2(-direction.x * speed, -direction.y * speed);
                 speed = 0.0f;
                 birdAmount--;
+                shot = true;
             }
         } 	
 	}
---
+++ PenguinThingy/Assets/score.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class score : MonoBehaviour {
+    [SerializeField]
+    int eggAmount = 10;
+    int points = 0;
+    [SerializeField]
+    Text PointsTE;
+    [SerializeField]
+    SlingShotController ssc;
+	// Use this for initialization
+	void Start () {
+        PointsTE.text = "Score: " + points;
+        ssc = GameObject.FindGameObjectWithTag("SlingShot").GetComponentInChildren<SlingShotController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (ssc.birdAmount == 0|| eggAmount == 0)
+        {
+            points += ssc.birdAmount;
+            Debug.Log(ssc.birdAmount);
+        }
+	}
+    public void EggHit()
+    {
+        eggAmount--;
+        points += 10;
+        Debug.Log(points);
+        PointsTE.text = "Score: " + points;
+        
+    }
+
+}
