9daf61383b494c23244313caaa7fc108c984eeaa
YDenker <33430355+YDenker@users.noreply.github.com>
2018-11-30 21:05:43 +0100

    Added Menu and Credits

---
+++ PenguinThingy/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour {
+
+    private void Start()
+    {
+        float currentAspect = (float)Screen.width / (float)Screen.height;
+        Camera.main.orthographicSize = 1920 / currentAspect / 200;
+    }
+
+    public void OnExit()
+    {
+        Application.Quit();
+
+    }
+
+    public void OnStartGame()
+    {
+        SceneManager.LoadScene("Character Controller Prototyping");
+    }
+
+    public void OnCreditScene()
+    {
+        SceneManager.LoadScene("CreditsScene");
+    }
+}
