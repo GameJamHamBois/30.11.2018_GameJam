4ab24d4220131c1d9bb02f30ca3247e7a05765d8
YDenker <33430355+YDenker@users.noreply.github.com>
2018-11-30 15:01:43 +0100

    Prototype UI
    sehr hässlich

---
+++ PenguinThingy/Assets/Script/Counter.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Counter : MonoBehaviour {
+
+    public Text TimeText;
+
+    private float timePassed = 0;
+
+	void Update ()
+    {
+        timePassed += Time.deltaTime;
+
+        TimeText.text = FormatTime(Mathf.FloorToInt(timePassed));
+	}
+
+    private string FormatTime(int time)
+    {
+        string extra = (time % 60) / 10 < 1 ? "0" : "";
+        string temp = (time / 60) / 10 < 1 ? "0" : "";
+        string penis = temp + (time/60).ToString() + ":" + extra + (time%60);
+        return penis;
+    }
+}
