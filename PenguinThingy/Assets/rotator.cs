bac235d873cdf7d5b727a73a7f7cfa33b595d9e9
Julian Heitkamp <heitkamp.julian@googlemail.com>
2018-11-30 14:02:09 +0100

    sligshot first try
    sligshot first try

---
+++ PenguinThingy/Assets/rotator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotator : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 diff = transform.position - Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        diff.Normalize();
+        float z_rot = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, z_rot+90);
+    }
+}
