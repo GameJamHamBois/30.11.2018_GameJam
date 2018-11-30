3bcc63a611cd32f9438e809515202bcc9f9cd20a
YDenker <33430355+YDenker@users.noreply.github.com>
2018-11-30 18:44:50 +0100

    Sun Added

---
+++ PenguinThingy/Assets/Scripts/Environment/SunController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SunController : MonoBehaviour {
+
+
+    public GameObject SunPrefab;
+    public Transform Pivot;
+    public Vector2 StartPosition;
+    public float TotalAngle;
+    public float TravelTime;
+
+    private float anglePerSec;
+
+    private GameObject sun;
+    private float currentTime = 0;
+
+	void Start ()
+    {
+        sun = GameObject.Instantiate(SunPrefab, new Vector3(StartPosition.x, StartPosition.y, 0),Quaternion.identity);
+        sun.transform.parent = Pivot.transform;
+        anglePerSec = TotalAngle / TravelTime;
+	}
+	
+	void Update ()
+    {
+        currentTime += Time.deltaTime;
+        if(currentTime < TravelTime)
+            Pivot.Rotate(0f, 0f, (anglePerSec * Time.deltaTime));
+
+	}
+}
